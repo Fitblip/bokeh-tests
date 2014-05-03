aa008e79c40d0c15e46eafad225f2c28cc9258a0
Fitblip <fitblip@gmail.com>
2014-05-02 23:14:22 -0700

    Yay excercises

--- style.py
+++ style.py
@@ -35,6 +35,7 @@ xs = [np.hstack((categories[::-1], categories))] * len(ys)
 colors = brewer["Spectral"][len(ys)]
 
 # EXERCISE: output static HTML file
+output_file("style.html")
 
 # EXERCISE: play around with parameters like:
 #   - line_color
@@ -44,20 +45,32 @@ colors = brewer["Spectral"][len(ys)]
 #   - fill_color
 #   - fill_alpha
 #   - background_fill
-patches(xs, ys, x_range=categories, y_range=Range1d(start=0, end=800),
-        color=colors, alpha=0.8, line_color=None, background_fill="lightgrey",
-        title="Categories of Brewering")
+patches(
+    xs,
+    ys,
+    x_range=categories,
+    y_range=Range1d(start=0, end=800),
+    color=colors,
+    alpha=0.8,
+    line_color="blue",
+    line_alpha=.5,
+    line_width=3,
+    line_dash=[2,3],
+    fill_color="purple",
+    background_fill="lightgrey",
+    title="Categories of Brewering"
+)
 
 # EXERCISE: configure all of the following plot properties
-ygrid().grid_line_color =            # color, or None, to suppress the line
-ygrid().grid_line_width =            # line width for grid lines
-axis().major_label_text_font_size =  # "12pt", "1.5em", "10px", etc
-axis().major_label_text_font_style = # "bold", "normal", "italic"
-axis().major_label_standoff =        # distance of tick labels from ticks
-axis().axis_line_color =             # color, or None, to suppress the line
-xaxis().major_label_orientation =    # radians, "horizontal", "vertical", "normal"
-xaxis().major_tick_in =              # distance ticks extends into the plot
-xaxis().major_tick_out =             # and distance they extend out
-xaxis().major_tick_line_color =      # color, or None, to suppress the line
+ygrid().grid_line_color = "blue"           # color, or None, to suppress the line
+ygrid().grid_line_width = "red"           # line width for grid lines
+axis().major_label_text_font_size = "10pt" # "12pt", "1.5em", "10px", etc
+axis().major_label_text_font_style = "bold" # "bold", "normal", "italic"
+axis().major_label_standoff =  1      # distance of tick labels from ticks
+axis().axis_line_color =   "brown"          # color, or None, to suppress the line
+xaxis().major_label_orientation = "horizontal"   # radians, "horizontal", "vertical", "normal"
+xaxis().major_tick_in =   2           # distance ticks extends into the plot
+xaxis().major_tick_out =   3          # and distance they extend out
+xaxis().major_tick_line_color = "black"     # color, or None, to suppress the line
 
 show()
